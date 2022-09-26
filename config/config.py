cc580b52e2ec31a2065ac4ce69f3343b5d8e15f4
“Antonio <tonyno@gmail.com>
2022-09-26 19:24:37 +0200

    prueba

--- config/config.py
+++ config/config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿# Carpeta a Observar
 cObservador = "D:\\FTP"
 
@@ -7,36 +6,16 @@ cDestino = "D:\\SUBIR"
 
 # Pantalla completa ¿?
 pCompleta = True
-=======
-# Carpeta a Observar
-cObservador = "pruebasObservador_orig"
-
-# Carpeta destino archivos una vez inspecciados
-cDestino = "pruebasObservador_dest"
-
-# Pantalla completa ¿?
-pCompleta = False
->>>>>>> parent of 776199c (Delete config.py)
 
 # Tamaño de la ventana
 tPantalla = "1280x1024"
 
 # Ubicacion de archivo LOG
-<<<<<<< HEAD
-ubicacionLog = 'D:\\Python\\Visor\\log'
+ubicacionLog = 'D:\\Python\\Visor_Q2\\log'
 nombreLog = 'Visor_log_Q2.txt'
 
 # Configuracion Ads Bechoff
 dirAds = '5.80.247.66.1.1'        ## Direccion
 portAds = 801                     ## Puerto
 velocidadScan = 0  
-=======
-ubicacionLog = '.\\log'
-nombreLog = 'Visor_log.txt'
-
-# Configuracion Ads Bechoff
-dirAds = '192.168.1.30.1.1'        ## Direccion
-portAds = 801                     ## Puerto
-velocidadScan = 0               
->>>>>>> parent of 776199c (Delete config.py)
 
